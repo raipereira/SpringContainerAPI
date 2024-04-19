a2136dbc12aa8c00e20837ddefc22eec0f597295
raipereira <raipereira84@gmail.com>
2024-04-19 15:08:31 -0500

    configure docker-compose

--- src/test/java/edu/miu/SC489/ads/ApiApplicationTests.java
+++ src/test/java/edu/miu/SC489/ads/ApiApplicationTests.java
@@ -1,10 +1,10 @@
-package miu.sc489.ads;
+package edu.miu.SC489.ads;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringContainerApiApplicationTests {
+class ApiApplicationTests {
 
     @Test
     void contextLoads() {
